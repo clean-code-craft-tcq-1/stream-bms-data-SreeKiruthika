7ff694544b33a6012550b9bd67dbc77d23c2ed8b
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-05-29 21:00:59 +0530

    Workspace with test and production code

--- main.c
+++ main.c
@@ -7,42 +7,31 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
+#include <signal.h> 
+#include <stdbool.h> 
 
-#include "test_mocks.h"
 #include "bms.h"
 
-float TemperatureInputTest = 0;
-float TemperatureOutputTest = 0;
-float ChargeRateInputTest = 0;
-float ChargeRateOutputTest = 0;
-int MAX_PRINT = 0;
-
-void TC_Print5()
-{
-    MAX_PRINT = 5;
-    BMSDataToConsoleSender();
-     /*validatePrintedOutput() does validations parallely on each printed output*/
-}
-
-void TC_Print15()
-{
-    MAX_PRINT = 15;
-    BMSDataToConsoleSender();
-     /*validatePrintedOutput() does validations parallely on each printed output*/
-}
-
-void TC_Print0()
-{
-    MAX_PRINT = 0;
-    BMSDataToConsoleSender();
-    /*validatePrintedOutput() does validations parallely on each printed output*/
-}
-
+bool sig_caught= false; 
+ 
+void signal_handler(int sig) 
+{ 
+  if (sig == SIGINT)
+  { 
+    sig_caught=true; 
+  } 
+} 
+ 
 int main()
 {
-    TC_Print5();
-    TC_Print15();
-    TC_Print0(); /*Null array*/
+    if (signal(SIGINT, signal_handler) == SIG_ERR) 
+    { 
+    fprintf(stderr, "signal func registration failed\n"); 
+    return 1; 
+    }
+    
+    BMSDataToConsoleSender();
+    
     return 0;
 }
 
