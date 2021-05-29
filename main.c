8ceb0fcb2c48fec0a165c90d1dfa9cb0a2b8bd17
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-05-29 22:45:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Main function
 *******************************************************************************/
 
 #include <stdio.h>
