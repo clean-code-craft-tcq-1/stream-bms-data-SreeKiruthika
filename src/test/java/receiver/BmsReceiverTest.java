44a6c992722ca5eb356a113d655ab4c635788f1c
rashika057 <rashika059@gmail.com>
2021-07-01 11:25:35 +0530

    Update BmsReceiverTest.java

--- src/test/java/receiver/BmsReceiverTest.java
+++ src/test/java/receiver/BmsReceiverTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import main.java.receiver.BmsReceiverTask;
 public class BmsReceiverTest {
 
 	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
